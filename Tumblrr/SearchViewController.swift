45fbe2ca44f4b83b40662c6fa53e2531f1b44b6c
Dorahan Arapgirlioglu <dorahann@gmail.com>
2016-02-26 14:21:58 -0800

    Added the optional features

--- Tumblrr/SearchViewController.swift
+++ Tumblrr/SearchViewController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    @IBOutlet weak var searchView: UIImageView!
+    @IBOutlet weak var loadingView: UIImageView!
 
+    var loading_1: UIImage!
+    var loading_2: UIImage!
+    var loading_3: UIImage!
+    
+    var images: [UIImage]!
+    var animatedImage: UIImage!
+    
+    var loaded = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +32,38 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        searchView.hidden = true
+        
+        loading_1 = UIImage(named: "loading-1")
+        loading_2 = UIImage(named: "loading-2")
+        loading_3 = UIImage(named: "loading-3")
+        
+        images = [loading_1, loading_2, loading_3]
+        
+        animatedImage = UIImage.animatedImageWithImages(images, duration: 1.0)
+        
+        loadingView.image = animatedImage
+        
+        
+            
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+    
+        if loaded == false {
+            delay(2){
+                self.loadingView.hidden = true
+                self.searchView.hidden = false
+                self.loaded = true
+            }
+        } else {
+            self.searchView.hidden = false
+        }
+    }
 
     /*
     // MARK: - Navigation
--- Tumblrr/TabBarViewController.swift
+++ Tumblrr/TabBarViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class TabBarViewController: UIViewController {
     @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var popUpView: UIImageView!
 
     @IBOutlet var buttons: [UIButton]!
     
@@ -22,6 +23,7 @@ class TabBarViewController: UIViewController {
     var viewControllers: [UIViewController]!
     
     var selectedIndex: Int = 0
+    var popUpDown = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +42,7 @@ class TabBarViewController: UIViewController {
         //Setting the initial tab for the button (since there is no tab selected yet)
         buttons[selectedIndex].selected = true
         didPressTab(buttons[selectedIndex])
+        
         
     }
 
@@ -76,8 +79,61 @@ class TabBarViewController: UIViewController {
         //Call the viewDidAppear method
         vc.didMoveToParentViewController(self)
         
+        
+        if selectedIndex == 1 {
+            UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: { () -> Void in
+                self.popUpView.alpha = 0
+                }, completion: { (Bool) -> Void in
+                self.popUpDown = false
+            })
+        } else {
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                if self.popUpDown == true {
+                self.popUpView.alpha = 1
+                }
+            })
+            
+            UIView.animateWithDuration(0.5, delay: 0, options: [.CurveEaseInOut, .Repeat, .Autoreverse], animations: { () -> Void in
+                if self.popUpDown == true {
+                    //self.popUpDown = false
+                    self.popUpView.center.y = 487
+                }
+                }, completion: { (Bool) -> Void in
+            })
+            
+        }
+        
+        
     }
-
     
+//    override func viewWillAppear(animated: Bool) {
+//        if selectedIndex == 1 {
+//            UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: { () -> Void in
+//                self.popUpView.alpha = 0
+//                }, completion: { (Bool) -> Void in
+//                    
+//            })
+//        } else {
+//            UIView.animateWithDuration(0.3, delay: 0, options: [.CurveEaseInOut, .Repeat], animations: { () -> Void in
+//                self.popUpView.alpha = 1
+//                if self.popUpDown == true {
+//                    self.popUpDown = false
+//                    self.popUpView.center.y = 417
+//                } else {
+//                    self.popUpDown = true
+//                    self.popUpView.center.y = 455
+//                    
+//                }
+//                }, completion: { (Bool) -> Void in
+//                    
+//            })
+//            
+//        }
+//    }
+    
+    
+    
+    
+
 
 }
